be809a9809fa841f6ea306585a27989b6d5b6005
kawena <kawena@163.com>
2020-07-26 21:53:48 +0800

    Spring dependency register learn

--- thinking-in-spring/dependency-source/src/main/java/org/cyg/thinking/in/spring/dependency/source/ResolvableDependencySourceDemo.java
+++ thinking-in-spring/dependency-source/src/main/java/org/cyg/thinking/in/spring/dependency/source/ResolvableDependencySourceDemo.java
@@ -1,8 +1,13 @@
 package org.cyg.thinking.in.spring.dependency.source;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.GenericBeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import javax.annotation.PostConstruct;
@@ -33,8 +38,17 @@ public class ResolvableDependencySourceDemo {
         });
         // 启动 Spring 应用上下文
         applicationContext.refresh();
-
+        // 刷新后，再注册, 会重置 DefaultListableBeanFactory.frozenBeanDefinitionNames 为 null
+        BeanDefinitionBuilder builder = BeanDefinitionBuilder.genericBeanDefinition(Integer.class);
+        builder.addConstructorArgValue(123);
+        applicationContext.registerBeanDefinition("test", builder.getBeanDefinition());
+        Integer test = applicationContext.getBean("test", Integer.class);
+        System.out.println(test);
+        for (String beanDefinitionName : applicationContext.getBeanDefinitionNames()) {
+            System.out.println("bean name:" + beanDefinitionName);
+        }
         // 显示地关闭 Spring 应用上下文
         applicationContext.close();
     }
+
 }
